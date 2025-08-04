4f8e4440c55c3bce6e02278606a0db14c3ae5734
rodrigueszkkk <kaikyauthentic@gmail.com>
2025-08-04 12:03:51 -0300

    modal pt1

--- script.js
+++ script.js
@@ -4,21 +4,26 @@ const cs = (el)=>document.querySelectorAll(el)
 pizzaJson.map((item, index)=> {
     let pizzaItem = c ('.models .pizza-item').cloneNode(true)
 
-
+    pizzaItem.setAttribute('data-key', index)
     pizzaItem.querySelector('.pizza-item--img img').src = item.img
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price}`
     pizzaItem.querySelector('a').addEventListener('click', (e)=>{
         e.preventDefault()
+        let key = e.target.closest('.pizza-item').getAttribute('data-key')
+
+        c('.pizzaInfo h1').innerHTML = pizzaJson[key].name
+        c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description
+        c('.pizzaBig img').src = pizzaJson[key].img
+        
+
 
         c('.pizzaWindowArea').style.opacity = 0
         c('.pizzaWindowArea').style.display = 'flex'
         setTimeout(()=>{
             c('.pizzaWindowArea').style.opacity = 1
         }, 200)
-        // c('.pizzaWindowArea').style.opacity = '1'
-
     })
 
 
