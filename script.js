0b42cc32d8582625f24968293ff09629869de2f9
rodrigueszkkk <kaikyauthentic@gmail.com>
2025-08-06 17:15:10 -0300

    cart mobile

--- script.js
+++ script.js
@@ -95,6 +95,15 @@ c('.pizzaInfo--addButton').addEventListener('click', ()=>{
     closeModal()
 })
 
+c('.menu-openner').addEventListener('click', ()=> {
+    if(cart.length > 0){
+        c('aside').style.left = '0'
+    }
+})
+c('.menu-closer').addEventListener('click', ()=> {
+    c('aside').style.left = '100vw'
+})
+
 function updateCart(){
     c('.menu-openner span').innerHTML = cart.length
 
@@ -159,6 +168,7 @@ function updateCart(){
         c('.total span:last-child').innerHTML = `R$ ${total.toFixed(2)}`
 
     } else {
-        c('aside').classList.remove('show')        
+        c('aside').classList.remove('show')  
+        c('aside').style.left = '100vw'   
     }
 }
