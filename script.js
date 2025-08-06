77ce39111cd12dc2bfccd19e936e7765150f6852
rodrigueszkkk <kaikyauthentic@gmail.com>
2025-08-06 14:33:49 -0300

    funcao carrinho

--- script.js
+++ script.js
@@ -79,11 +79,8 @@ cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
 })
 c('.pizzaInfo--addButton').addEventListener('click', ()=>{
     let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'))
-
     let identifier = pizzaJson[modalKey].id+"@"+size
-
     let key = cart.findIndex((item)=>item.identifier == identifier)
-
     if(key > -1){
         cart[key].qt += modalQt
     } else {
@@ -94,7 +91,22 @@ c('.pizzaInfo--addButton').addEventListener('click', ()=>{
             qt:modalQt
         })
     }
-
+    updateCart()
     closeModal()
 })
 
+function updateCart(){
+    if(cart.length > 0){
+        c('aside').classList.add('show')
+        for(let i in cart) {
+
+            let pizzaItem = pizzaJson.find((item)=> item.id == cart[i].id)
+
+            console.log(pizzaItem)
+
+        }
+
+    } else {
+        c('aside').classList.remove('show')        
+    }
+}
