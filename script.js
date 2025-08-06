7904e1df464710da6593bc6dd5c2590fa95be017
rodrigueszkkk <kaikyauthentic@gmail.com>
2025-08-06 16:57:38 -0300

    fix cart bug

--- script.js
+++ script.js
@@ -96,10 +96,24 @@ c('.pizzaInfo--addButton').addEventListener('click', ()=>{
 })
 
 function updateCart(){
+    c('.menu-openner span').innerHTML = cart.length
+
     if(cart.length > 0){
         c('aside').classList.add('show')
+        c('.cart').innerHTML = ''
+
+
+        let subtotal = 0
+        let desconto = 0
+        let total = 0
+
         for(let i in cart) {
+
             let pizzaItem = pizzaJson.find((item)=> item.id == cart[i].id)
+            
+            subtotal += pizzaItem.price * cart[i].qt
+
+
             let cartItem = c('.models .cart--item').cloneNode(true)
 
             let pizzaSizeName
@@ -121,9 +135,28 @@ function updateCart(){
             cartItem.querySelector('img').src = pizzaItem.img
             cartItem.querySelector('.cart--item-nome').innerHTML = pizzaItem = pizzaName
             cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt
-
+            cartItem.querySelector('.cart--item-qtmenos').addEventListener('click', ()=>{
+                if(cart[i].qt > 1){
+                    cart[i].qt--
+                } else {
+                    cart.splice(i,1)
+                }
+                updateCart()
+            })
+            cartItem.querySelector('.cart--item-qtmais').addEventListener('click', ()=>{
+                cart[i].qt++
+                updateCart()
+            })
+        
             c('.cart').append(cartItem)
         }
+
+        desconto = subtotal * 0.1
+        total = subtotal - desconto
+
+        c('.subtotal span:last-child').innerHTML = `R$ ${subtotal.toFixed(2)}`
+        c('.desconto span:last-child').innerHTML = `R$ ${desconto.toFixed(2)}`
+        c('.total span:last-child').innerHTML = `R$ ${total.toFixed(2)}`
 
     } else {
         c('aside').classList.remove('show')        
